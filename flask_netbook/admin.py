b6d8d4248a89c16c1d866919f789c48fea327462
Dommert <DommertEnterprises@Gmail.com>
2014-08-05 22:43:58 -0700

    updates

--- flask_netbook/admin.py
+++ flask_netbook/admin.py
@@ -4,11 +4,26 @@
 from flask import Flask
 
 from app import app, db, auth
-from models import *
-from flask_turboduck.admin import Admin
+from models import Note, User
+from flask_turboduck.admin import Admin, ModelAdmin
 
 
+# Admin Models
+
+class MessageAdmin(ModelAdmin):
+    columns = ('user', 'content', 'pub_date',)
+    foreign_key_lookups = {'user': 'username'}
+
+class UserAdmin(ModelAdmin):
+    columns = ('username', 'email', 'is_superuser',)
+
+
+
+
+# Register
 admin = Admin(app, db)
 admin.register(Note)
-
+admin.register(User, UserAdmin)
 admin.setup()
+
+
